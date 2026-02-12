8233ec5d7440bf5701cfbd677a6a33b887e799f8
woojin-heo <woojin.heo@gmail.com>
2026-02-12 18:19:24 +0800

    add tools

---
+++ backend/agent/tools/__init__.py
@@ -0,0 +1,3 @@
+from .general_tools import wikipedia_search, tavily_search
+
+__all__ = ["wikipedia_search", "tavily_search"]
---
+++ backend/agent/tools/general_tools.py
@@ -0,0 +1,39 @@
+import os
+from dotenv import load_dotenv
+from langchain_community.document_loaders import WikipediaLoader
+from langchain_tavily import TavilySearch
+
+load_dotenv()
+
+def wikipedia_search(query: str, load_max_docs: int = 3) -> str:
+    '''
+    Search Wikipedia for the given query and return the results.
+    Args:
+        query: The query to search for.
+        load_max_docs: The maximum number of documents to load.
+    Returns:
+        A string containing the search results.
+    '''
+    search_docs = WikipediaLoader(query=query, load_max_docs=load_max_docs).load()
+    formatted_search_results = [
+        f'<Document source="{doc.metadata["source"]}">\n{doc.page_content}\n</Document>'
+        for doc in search_docs
+    ]
+    return "\n\n-----\n\n".join(formatted_search_results)
+
+def tavily_search(query: str, load_max_docs: int = 3) -> str:
+    '''
+    Search Tavily for the given query and return the results.
+    Args:
+        query: The query to search for.
+        load_max_docs: The maximum number of documents to load.
+    Returns:
+        A string containing the search results.
+    '''
+    tavily_search = TavilySearch(max_results=load_max_docs, include_images=False, include_image_descriptions=False)
+    search_docs = tavily_search.run(query)
+    formatted_search_results = [
+        f'<Document source="{doc["url"]}"> title: {doc["title"]}\n{doc["content"]}\n</Document>'
+        for doc in search_docs['results']
+    ]
+    return "\n\n-----\n\n".join(formatted_search_results)
